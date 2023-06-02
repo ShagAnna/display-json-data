c4c8390cc282d414703e69eeb3834e10e051ae3f
anna.shagoyan <shagoyananna@gmail.com>
2023-06-02 20:29:12 +0300

    added logic for displaying datain html

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
+const tableBody = document.querySelector('.tbody');
+
 fetch ('data.json').then(res => res.json())
   .then(filterUsersData);
 
 function filterUsersData(users) {
+  let all_users = [];
+
   {
     let total_amount = 0;
     total_amount += users[0].purchase_history[0].quantity;
     total_amount += users[0].purchase_history[1].quantity;
     total_amount += users[0].purchase_history[2].quantity;
 
-    let user1 = {
+    all_users.push({
       name: users[0].name,
       email: users[0].email,
       unique_purchase_amount: users[0].purchase_history.length,
       total_amount: total_amount
-    };
+    });
 
-    document.querySelector('.name').append(JSON.stringify(user1.name));
-    document.querySelector('.email').append(JSON.stringify(user1.email));
-    document.querySelector('.product').append(JSON.stringify(user1.unique_purchase_amount));
-    document.querySelector('.total').append(JSON.stringify(user1.total_amount));
-    console.log(user1)
+    console.log(typeof total_amount)
   }
 
   {
@@ -27,15 +27,14 @@ function filterUsersData(users) {
     total_amount += users[1].purchase_history[0].quantity;
     total_amount += users[1].purchase_history[1].quantity;
     total_amount += users[1].purchase_history[2].quantity;
+    total_amount += users[1].purchase_history[3].quantity;
 
-    let user2 = {
+    all_users.push({
       name: users[1].name,
       email: users[1].email,
       unique_purchase_amount: users[1].purchase_history.length,
       total_amount: total_amount
-    };
-
-    console.log(user2)
+    });
   }
 
   {
@@ -43,21 +42,49 @@ function filterUsersData(users) {
     total_amount += users[2].purchase_history[0].quantity;
     total_amount += users[2].purchase_history[1].quantity;
     total_amount += users[2].purchase_history[2].quantity;
+    total_amount += users[2].purchase_history[3].quantity;
 
-    let user3 = {
+    all_users.push({
       name: users[2].name,
       email: users[2].email,
       unique_purchase_amount: users[2].purchase_history.length,
       total_amount: total_amount
-    };
-
-    //document.querySelector('.users').append(JSON.stringify(user3));
-    console.log(user3)
+    });
   }
 
+  for (const user of all_users) {
+    const tableRow = createTableRow(user);
+    tableBody.appendChild(tableRow);
+  }
+}
 
+function createTableRow(data) {
+  const row = document.createElement('tr');
+
+  const nameCell = document.createElement('td');
+  nameCell.innerHTML = data.name;
+  row.appendChild(nameCell);
+
+  const emailCell = document.createElement('td');
+  emailCell.innerHTML = data.email;
+  row.appendChild(emailCell);
+
+  const productCell = document.createElement('td');
+  productCell.innerHTML = data.unique_purchase_amount;
+  row.appendChild(productCell);
+  console.log(data.unique_purchase_amount)
+
+  const totalCell = document.createElement('td');
+  totalCell.innerHTML = data.total_amount;
+  row.appendChild(totalCell);
+
+  return row;
 
 }
+
+
+
+
 
 
 
